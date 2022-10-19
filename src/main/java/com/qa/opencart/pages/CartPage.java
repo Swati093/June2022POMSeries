f719575570692cdda4e2ef11b695441dfc90be52
Swati093 <swatigoswami093@gmail.com>
2022-10-19 10:23:34 +0530

    updated code on the basis of review comment

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,6 +1,14 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
 	String product="iphone";
+	
+	By loc=By.id("product");
+	
+	public void click() {
+		System.out.println("click---"+loc);
+	}
 }
